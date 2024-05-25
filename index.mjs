70a00f9c1a2be50ac5d2e6d1b5c0c1bd154b391c
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-05-25 16:57:53 -0700

    v0.1.1

--- index.mjs
+++ index.mjs
@@ -1,12 +1,13 @@
-import express from 'express';
-import { createBareServer } from '@tomphttp/bare-server-node';
+import fastify from 'fastify';
+import { fastifyStatic } from '@fastify/static';
 import { createServer } from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
 import path, { dirname } from 'node:path';
 import createRammerhead from 'rammerhead/src/server/index.js';
 import { fileURLToPath } from 'node:url';
 import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
 
-const app = express();
+const server = createServer();
 const bare = createBareServer('/~/bare/');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -50,8 +51,8 @@ function routeRammerheadUpgrade(req, socket, head) {
 }
 
 const serverFactory = (handler, opts) => {
-    return createServer()
-      .on('request', (req, res) => {
+    return server
+        .on('request', (req, res) => {
             if (bare.shouldRoute(req)) {
                 bare.routeRequest(req, res);
             } else if (shouldRouteRammerhead(req)) {
@@ -59,20 +60,29 @@ const serverFactory = (handler, opts) => {
             } else {
                 handler(req, res);
             }
-      })
-      .on('upgrade', (req, socket, head) => {
+        })
+        .on('upgrade', (req, socket, head) => {
             if (bare.shouldRoute(req)) {
                 bare.routeUpgrade(req, socket, head);
             } else if (shouldRouteRammerhead(req)) {
                 routeRammerheadUpgrade(req, socket, head);
             }
-      });
+        });
 };
 
-app.use(express.static(path.join(__dirname, 'public')));
+const app = fastify({ logger: true, serverFactory });
 
-app.use('/~/uv/', express.static(uvPath));
+app.register(fastifyStatic, {
+    root: path.join(__dirname, 'public'),
+    decorateReply: false
+});
 
-if (serverFactory) {
-    serverFactory(app);
-}
+app.register(fastifyStatic, {
+    root: uvPath,
+    prefix: '/uv/',
+    decorateReply: false
+});
+
+app.listen({
+    port: 8080
+});
