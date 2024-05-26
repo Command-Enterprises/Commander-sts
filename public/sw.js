166da15627185f8d1c1f1bbd30c8590dfea22641
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-05-26 07:16:23 -0700

    v0.1.13 (smug commit)

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,27 @@
-importScripts("/dy/config.js?v=12");
-importScripts("/dy/worker.js?v=12");
-importScripts("/assets/-/bundle.js?v=5-5-2024");
-importScripts("/assets/-/config.js?v=5-5-2024");
-importScripts(__uv$config.sw || "/assets/-/sw.js?v=2");
+importScripts("/dynamic/dynamic.config.js?v=12");
+importScripts("/dynamic/dynamic.worker.js?v=12");
+importScripts("/uv/uv.bundle.js?v=5-5-2024");
+importScripts("/uv/uv.config.js?v=5-5-2024");
+importScripts(__uv$config.sw || "/uv/uv.sw.js?v=2");
 
-const uv = new UVServiceWorker()
-const dynamic = new Dynamic()
+const uv = new UVServiceWorker();
+const dynamic = new Dynamic();
 
-let userKey = new URL(location).searchParams.get("userkey")
-self.dynamic = dynamic
+let userKey = new URL(location).searchParams.get("userkey");
+self.dynamic = dynamic;
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async function () {
       if (await dynamic.route(event)) {
-        return await dynamic.fetch(event)
+        return await dynamic.fetch(event);
       }
 
-      if (event.request.url.startsWith(location.origin + "/a/")) {
-        return await uv.fetch(event)
+      if (event.request.url.startsWith(location.origin + "/~/uv/")) {
+        return await uv.fetch(event);
       }
 
-      return await fetch(event.request)
+      return await fetch(event.request);
     })()
-  )
+  );
 });
